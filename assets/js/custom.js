743688f751537ac98602505d388731c822a8265f
jelatz <jladaudrey@gmail.com>
2023-04-07 01:20:11 +0800

    initial admin dashboard

---
+++ assets/js/custom.js
@@ -0,0 +1,9 @@
+function forgotPass(){
+  document.getElementById('login').style.display='none';
+  document.getElementById('forgot').style.display='flex';
+}
+
+function login (){
+    document.getElementById('login').style.display='flex';
+  document.getElementById('forgot').style.display='none';
+}
