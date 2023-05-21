0dc8fc3a7ecd340ec6382f2fe6e331e87b9dc051
jelatz <jladaudrey@gmail.com>
2023-05-22 04:29:15 +0800

    completed accounts and events

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,11 +7,46 @@ function login (){
     document.getElementById('login').style.display='flex';
   document.getElementById('forgot').style.display='none';
 }
-
 function themeChange() {
-  var element = document.body;
-  element.classList.toggle("dark-mode");
+  var body = document.body;
+  var themeLink = document.querySelector(".nav-link[name='themeChange']");
+  var welcome = document.getElementsByClassName('welcome-message');
+  var tables = document.getElementsByClassName("table");
+  var navbar = document.getElementsByClassName('navbar');
+  var nav = document.getElementsByClassName('nav-link');
+  
+  if (body.classList.contains("dark-mode")) {
+    body.classList.remove("dark-mode");
+    themeLink.textContent = "Dark Mode";   
+    for (var i = 0; i < tables.length; i++) {
+      tables[i].style.color = 'black';
+    }  
+    for (var i = 0; i < nav.length; i++) {
+      nav[i].style.color = "black";
+    }
+    for (var i = 0; i < navbar.length; i++) {
+      navbar[i].style.backgroundColor = "white";
+    }
+  } else {
+    body.classList.add("dark-mode");
+    themeLink.textContent = "Light Mode";
+    // for (var i = 0; i < welcome.length; i++) {
+    //   welcome[i].style.color = 'black';
+    // }
+    for (var i = 0; i < tables.length; i++) {
+      tables[i].style.color = 'white';
+    }
+    for (var i = 0; i < navbar.length; i++) {
+      navbar[i].style.backgroundColor = "black";
+    }
+    for (var i = 0; i < nav.length; i++) {
+      nav[i].style.color = "white";
+    }
+    
+  }
 }
+
+
 
 var account = document.getElementById("account");
 var children = account.children;
